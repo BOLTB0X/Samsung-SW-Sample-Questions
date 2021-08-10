6a8ccdf92f0fed454f3382eb1006c82bcf00583e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-08-10 10:12:10 +0900

    로봇청소기
    https://www.acmicpc.net/problem/14503

--- 14503_로봇청소기.cpp
+++ 14503_로봇청소기.cpp
@@ -3,87 +3,89 @@
 using namespace std;
 
 int n, m;
+int map[51][51];
 queue<pair<pair<int, int>, int>> que;
-int map[50][50];
-
-int dy[4] = { -1,0,1,0 };
-int dx[4] = { 0,1,0,-1 };
-
-//좌회전
-int turn_left(int d) {
-	if (d == 0)
+int dr[4] = { -1,0,1,0 };
+int dc[4] = { 0,1,0,- 1 };
+//방향
+//  0
+// 3 1
+//  2
+// 좌회전
+int turn_left(int dir) {
+	if (dir == 0)
 		return 3;
-	else if (d == 1)
-		return 0;
-	else if (d == 2)
-		return 1;
-	else if (d == 3)
+	else if (dir == 3)
 		return 2;
-}
-//후진 
-int turn_back(int d) {
-	if (d == 0)
-		return 2;
-	else if (d == 1)
-		return 3;
-	else if (d == 2)
+	else if (dir == 1)
 		return 0;
-	else if (d == 3)
+	else if (dir ==2)
 		return 1;
 }
-
+//후진
+int turn_back(int dir) {
+	if (dir == 0)
+		return 2;
+	else if (dir == 3)
+		return 1;
+	else if (dir == 1)
+		return 3;
+	else if (dir == 2)
+		return 0;
+}
+//시뮬레이션 BFS이용
 int BFS(int r, int c, int d) {
-	int ret = 1;
-	//1.현재 위치를 청소
-	map[r][c] = 2; //청소처리
-	que.push({ {r, c}, d });
-
+	que.push({ {r,c},d });
+	int result = 1;
+	map[r][c] = 2; //1.현재위치 청소
+	
 	while (!que.empty()) {
 		int cur_r = que.front().first.first;
 		int cur_c = que.front().first.second;
 		int cur_d = que.front().second;
-		int tmp_d = cur_d;
+		int tmp_d = cur_d; //방향을 자주 바꿔야 하므로
 		que.pop();
-
-		//2.왼쪽방향부터 차례대로 탐색
+		 
+		//2.현재 위치에서 현재 방향을 기준으로 왼쪽 방향부터 차례대로 인접한 칸을 탐색
 		for (int i = 0; i < 4; i++) {
-			//2.b 조건
+			//2.b
 			tmp_d = turn_left(tmp_d);
-			int move_r = cur_r + dy[tmp_d];
-			int move_c = cur_c + dx[tmp_d];
-
-			//2.a 조건
-			if (0 <= move_c && move_c < m && 0 <= move_r && 
-				move_r < n && map[move_r][move_c] == 0) {
-				ret += 1;
-				map[move_r][move_c] = 2; //1.현재 위치 청소
-				que.push({ {move_r, move_c}, tmp_d });
+			int move_r = cur_r + dr[tmp_d];
+			int move_c = cur_c + dc[tmp_d];
+			//2.a
+			if (0 <= move_r && move_r < n
+				&& 0 <= move_c && move_c < m
+				&& map[move_r][move_c] == 0) {
+				map[move_r][move_c] = 2;
+				que.push({ {move_r,move_c},tmp_d });
+				result += 1;
 				break;
 			}
-			//2.c 조건
+			//2.c
 			else if (i == 3) {
-				int move_r = cur_r + dy[turn_back(cur_d)];
-				int move_c = cur_c + dx[turn_back(cur_d)];
-				que.push({ {move_r, move_c}, cur_d });
-				//2.d 조건
-				if (map[move_r][move_c] == 1)
-					return ret;
+				int back_r = cur_r + dr[turn_back(cur_d)];
+				int back_c = cur_c + dc[turn_back(cur_d)];
+				que.push({ {back_r,back_c},cur_d });
+				//2.d
+				if (map[back_r][back_c] == 1) {
+					return result;
+				}
 			}
 		}
 	}
 }
-
-int main() {
-	//입력
+int main(void) {
 	cin >> n >> m;
-	int row, col, dir;
-	cin >> row >> col >> dir;
 
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++)
-			cin >> map[i][j];
+	int direction, row, col;
+	cin >> row >> col >> direction;
+
+	//맵생성
+	for (int r = 0; r < n; r++) {
+		for (int c = 0; c < m; c++) {
+			cin >> map[r][c];
+		}
 	}
-
-	cout << BFS(row, col, dir) << '\n';
+	cout << BFS(row, col, direction) << '\n';
 	return 0;
 }
