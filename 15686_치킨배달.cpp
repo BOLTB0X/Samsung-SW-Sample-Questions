a352b41a6dd1d8edf224e1dd2b95be1dec13601a
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-08 13:58:05 +0900

    치킨배달
    https://www.acmicpc.net/problem/15686

--- 15686_치킨배달.cpp
+++ 15686_치킨배달.cpp
@@ -4,57 +4,63 @@
 using namespace std;
 
 const int INF = 0x7fffffff;
-int n, m;
-int result;
-int graph[50][50];
-vector<pair<int, int>> house, chicken;
-bool visited[13];
+int n, m, result;
+int map[51][51];
+vector<pair<int, int>> house;
+vector<pair<int, int>> chicken;
+bool visited[14];
 
-int distance(pair<int, int> a, pair<int, int> b) {
-    return abs(a.first - b.first) + abs(a.second - b.second);
+int manhattan_dist(pair<int, int> a, pair<int, int> b) {
+	return abs(a.first - b.first) + abs(a.second - b.second);
 }
 
-void DFS(int idx, int selected) {
-    //조건 만족
-    if (selected == m) {
-        int tempResult = 0;
-        for (int i = 0; i < house.size(); i++) {
-            int dist = INF;
-            for (int j = 0; j < chicken.size(); j++)
-                if (visited[j])
-                    dist = min(dist, distance(house[i], chicken[j]));
-            tempResult += dist;
-        }
-        result = min(result, tempResult);
-        return;
-    }
+int get_chicken_dist() {
+	int tmp_result = 0;
+	for (int i = 0; i < house.size(); i++) {
+		int dist = INF;
+		for (int j = 0; j < chicken.size(); j++) {
+			if (visited[j])
+				dist = min(dist, manhattan_dist(house[i], chicken[j]));
+		}
+		tmp_result += dist;
+	}
+	return tmp_result;
+}
 
-    //기저 사례
-    if (idx == chicken.size()) return;
+void combination(int idx, int select_cnt) {
+	if (select_cnt == m) {
+		result = min(result, get_chicken_dist());
+		return;
+	}
 
-    //프랜차이즈 선정
-    visited[idx] = true;
-    DFS(idx + 1, selected + 1);
-    //프랜차이즈 선정 X
-    visited[idx] = false;
-    DFS(idx + 1, selected);
+	if (idx == chicken.size())
+		return;
+	visited[idx] = true;
+	combination(idx + 1, select_cnt + 1);
+	visited[idx] = false;
+	combination(idx + 1, select_cnt);
 }
 
 int main(void) {
-    cin >> n >> m;
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0); cout.tie(0);
 
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < n; j++) {
-            cin >> graph[i][j];
-            if (graph[i][j] == 1)
-                house.push_back({ i, j });
-            else if (graph[i][j] == 2)
-                chicken.push_back({ i, j });
-        }
-    }
+	//입력
+	cin >> n >> m;
 
-    result = INF;
-    DFS(0, 0);
-    cout << result << "\n";
-    return 0;
-} 
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < n; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == 1)
+				house.push_back({ i,j });
+			else if (map[i][j] == 2)
+				chicken.push_back({ i,j });
+		}
+	}
+
+	result = INF;
+	combination(0, 0);
+	cout << result << "\n";
+	return 0;
+}
