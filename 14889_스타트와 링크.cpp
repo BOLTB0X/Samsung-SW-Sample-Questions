592a3b18d2285e6066efcf1ee0813ebef8459c67
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-21 14:54:13 +0900

    스타트와 링크
    https://www.acmicpc.net/problem/14889

--- 14889_스타트와 링크.cpp
+++ 14889_스타트와 링크.cpp
@@ -1,16 +1,27 @@
-#include <iostream>
-#include <math.h>
-using namespace std;
+#define _CRT_SECURE_NO_WARNINGS
+#include <cstdio>
 
 int n;
 int map[21][21];
 bool visited[22];
-int min_value = 0x7fffffff;
+int result = 0x7fffffff;
+
+//최소
+int min(int a, int b) {
+	return a < b ? a : b;
+}
+
+//절댓값
+int abs(int a) {
+	if (a < 0)
+		return -a;
+	return a;
+}
 
 void DFS(int cnt, int cur) {
-	if (cnt == n/2) {
-		int start=0;
-		int link=0;
+	if (cnt == n / 2) {
+		int start = 0;
+		int link = 0;
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= n; j++) {
 				if (visited[i] && visited[j]) {
@@ -21,25 +32,25 @@ void DFS(int cnt, int cur) {
 				}
 			}
 		}
-		min_value = min(abs(start - link), min_value);
+		result = min(abs(start - link), result);
 		return;
 	}
 	for (int i = cur; i < n; i++) {
 		visited[i] = true;
-		DFS(cnt + 1, i+1);
+		DFS(cnt + 1, i + 1);
 		visited[i] = false;
 	}
 }
 
 int main(void) {
-	cin >> n;
+	scanf("%d", &n);
 
 	for (int y = 1; y <= n; y++) {
 		for (int x = 1; x <= n; x++) {
-			cin >> map[y][x];
+			scanf("%d", &map[y][x]);
 		}
 	}
 	DFS(0, 1);
-	cout << min_value << '\n';
+	printf("%d", result);
 	return 0;
 }
