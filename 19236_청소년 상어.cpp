4fe6a801c4b00e93ac8b6ffff26c6d6430dbac67
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-24 16:35:40 +0900

    청소년 상어
    https://www.acmicpc.net/problem/19236

--- 19236_청소년 상어.cpp
+++ 19236_청소년 상어.cpp
@@ -1,20 +1,19 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 
-using namespace std;
-
-//물고기 자료형
+//물고기 구조체
 struct FISH {
-	int y, x, dir, alive; //1 살아있음, 0 죽음
+	//죽었으면 0, 살아있으면1
+	int y, x, dir, alive;
 };
 
-int map[4][4];
+int result;
+int board[5][5];
 FISH fish[17];
 
 //이동방향
 const int dy[9] = { 0,-1,-1,0,1,1,1,0,-1 };
-const int dx[9] = { 0,0,-1,-1,-1,0,1,1,1 };
-int result;
+const int dx[9] = { 0, 0,-1,-1,-1,0,1,1,1 };
 
 //최대
 int max(int a, int b) {
@@ -22,9 +21,9 @@ int max(int a, int b) {
 }
 
 //맵 복사
-void map_backup(int(*from)[4], int(*to)[4]) {
-	for (int y = 0; y < 4; y++) {
-		for (int x = 0; x < 4; x++) {
+void board_backup(int(*from)[5], int(*to)[5]) {
+	for (int y = 1; y <= 4; y++) {
+		for (int x = 1; x <= 4; x++) {
 			to[y][x] = from[y][x];
 		}
 	}
@@ -32,7 +31,7 @@ void map_backup(int(*from)[4], int(*to)[4]) {
 }
 
 //배열 복사
-void arr_backup(FISH* from, FISH* to) {
+void fish_backup(FISH* from, FISH* to) {
 	for (int i = 1; i <= 16; i++) {
 		to[i] = from[i];
 	}
@@ -40,7 +39,7 @@ void arr_backup(FISH* from, FISH* to) {
 }
 
 //물고기들의 좌표를의 스와핑
-void fish_swap(int a, int b) {
+void arr_swap(int a, int b) {
 	FISH tmp = fish[a];
 	fish[a].y = fish[b].y;
 	fish[a].x = fish[b].x;
@@ -49,13 +48,14 @@ void fish_swap(int a, int b) {
 	return;
 }
 
-//물고기들의 이동 
-void move_fish(void) {
-	//물고기들 작은 순번부터
+//맵상의 물고기들 이동
+void fish_move(void) {
+	//배열 탐색
 	for (int i = 1; i <= 16; i++) {
-		//죽었다면
+		//죽어있다면
 		if (fish[i].alive == 0)
 			continue;
+		//현재 물고기 정보
 		int y = fish[i].y;
 		int x = fish[i].x;
 		int dir = fish[i].dir;
@@ -63,131 +63,146 @@ void move_fish(void) {
 		int ny = y + dy[dir];
 		int nx = x + dx[dir];
 		bool flag = false;
-		//범위 내고
-		if (ny >= 0 && nx >= 0 && ny < 4 && nx < 4) {
-			//이동 가능 하면
-			if (map[ny][nx] == 0) {
+
+		//범위 내
+		if (ny <= 4 && nx <= 4 && ny >= 1 && nx >= 1) {
+			//이동 가능한 경우
+			if (board[ny][nx] == 0) {
 				flag = true;
 				fish[i].y = ny;
 				fish[i].x = nx;
-				map[ny][nx] = i;
-				map[y][x] = 0;
+				board[ny][nx] = i;
+				//이동했으니 비어있는 공간
+				board[y][x] = 0;
 			}
-			//다른 물고기가 있으면
-			else if (map[ny][nx] != -1) {
+			//다른 물고기가 있다면
+			else if (board[ny][nx] != -1) {
 				flag = true;
-				fish_swap(i, map[ny][nx]);
-				swap(map[y][x], map[ny][nx]);
+				//서로 위치 변경
+				arr_swap(i, board[ny][nx]);
+				swap(board[y][x], board[ny][nx]);
 			}
 		}
-		//여기까지 와서 flag가 거짓인 경우
-		//물고기들은 현재방향으로 진행불능
-		if (flag==false) {
+		//물고기들이 이동할 수 없을 경우
+		if (!flag) {
+			//45도 반시계방향 회전
 			int nd = dir + 1;
 			if (nd == 9)
 				nd = 1;
 			int ny = y + dy[nd];
 			int nx = x + dx[nd];
 
+			//360도로 돌기전까지
 			while (nd != dir) {
-				if (ny >= 0 && nx >= 0 && ny < 4 && nx < 4) {
-					if (map[ny][nx] == 0) {
+				//범위 내
+				if (ny <= 4 && nx <= 4 && ny >= 1 && nx >= 1) {
+					//이동 가능한 경우
+					if (board[ny][nx] == 0) {
 						fish[i].y = ny;
 						fish[i].x = nx;
-						map[ny][nx] = i;
-						map[y][x] = 0;
+						board[ny][nx] = i;
+						//이동했으니 비어있는 공간
+						board[y][x] = 0;
 						fish[i].dir = nd;
 						break;
 					}
-					else if (map[ny][nx] != -1) {
-						fish_swap(i, map[ny][nx]);
-						swap(map[y][x], map[ny][nx]);
+					//다른 물고기가 있다면
+					else if (board[ny][nx] >= 1) {
+						//서로 위치 변경
+						arr_swap(i, board[ny][nx]);
+						swap(board[ny][nx], board[y][x]);
 						fish[i].dir = nd;
 						break;
 					}
 				}
-			nd++;
-			if (nd == 9)
-				nd = 1;
-			ny = y + dy[nd];
-			nx = x + dx[nd];
+				//아닐수 차례로 돌린다.
+				nd++;
+				if (nd == 9) 
+					nd = 1;
+				ny = y + dy[nd];
+				nx = x + dx[nd];
 			}
 		}
 	}
+	return;
 }
 
 //물고기들 상태
 void fish_state(int y, int x, int ny, int nx, int fish_idx, bool flag) {
 	if (flag == true) {
-		map[y][x] = 0;
+		board[y][x] = 0;
 		//상어 위치
-		map[ny][nx] = -1;
+		board[ny][nx] = -1;
 		fish[fish_idx].alive = 0;
 	}
 	else {
-		map[y][x] = -1;
+		board[y][x] = -1;
 		//상어 위치 그래도 위치
-		map[ny][nx] = fish_idx;
+		board[ny][nx] = fish_idx;
 		fish[fish_idx].alive = 1;
 	}
 	return;
 }
 
-//상어로 백트래킹
-void DFS(int shark_y, int shark_x, int tot, int shark_dir) {
+//백트래킹을 위한 DFS
+void DFS(int sy, int sx, int tot, int sdir) {
 	result = max(result, tot);
-	//백트래킹이므로 tmp선언
-	int c_map[4][4];
+	//백업용
+	int c_board[5][5];
 	FISH c_fish[17];
-	//백업시켜두기
-	map_backup(map, c_map);
-	arr_backup(fish, c_fish);
-	
+	board_backup(board, c_board);
+	fish_backup(fish, c_fish);
+
 	//물고기들 이동
-	move_fish();
-
-	//상어 이동
+	fish_move();
+	//상어들 이동
+	//고려할 것이 이동하면 물고기를 먹거나 안먹거나
 	for (int i = 1; i <= 3; i++) {
-		int ny = shark_y + dy[shark_dir] * i;
-		int nx = shark_x + dx[shark_dir] * i;
+		int ny = sy + dy[sdir] * i;
+		int nx = sx + dx[sdir] * i;
 
-		if (ny >= 0 && nx >= 0 && ny < 4 && nx < 4) {
-			if (map[ny][nx] == 0)
-				continue;
-			int fish_idx = map[ny][nx];
+		//범위 내
+		if (ny <= 4 && nx <= 4 && ny >= 1 && nx >= 1) {
+			if (board[ny][nx] == 0) continue;
+			int fish_idx = board[ny][nx];
 			int nd = fish[fish_idx].dir;
 
-			//상어가 이동하며 먹은 경우와 안먹은 경우
-			fish_state(shark_y, shark_x, ny, nx, fish_idx, true);
+			//상어가 이동하며 먹는 경우와 안먹는 경우
+			fish_state(sy, sx, ny, nx, fish_idx, true);
 			DFS(ny, nx, tot + fish_idx, nd);
-			fish_state(shark_y, shark_x, ny, nx, fish_idx, false);
+			fish_state(sy, sx, ny, nx, fish_idx, false);
 		}
 		else
 			break;
 	}
-	map_backup(c_map, map);
-	arr_backup(c_fish, fish);
+	board_backup(c_board, board);
+	fish_backup(c_fish, fish);
 }
 
-//시뮬
+//시뮬레이션 시작
 void simulation(void) {
-	//초기 상어가 (0,0)물고기 잡아먹음
-	int fish_idx = map[0][0];
-	int fish_dir = fish[fish_idx].dir;
-	fish[fish_idx].alive = 0;
-	//상어 위치 
-	map[0][0] = -1; 
-	//백트래킹 
-	DFS(0, 0, fish_idx, fish_dir);
+	//상어는 무조건 초기 (1,1)
+	//방향은 잡아먹은 물고기 방향
+	result = 0;
+	int shark_idx = board[1][1];
+	int shark_dir = fish[shark_idx].dir;
+	fish[shark_idx].alive = 0;
+	//상어는 -1로
+	board[1][1] = -1;
+	//백트래킹 시작
+	DFS(1, 1, shark_idx, shark_dir);
 	return;
 }
+
 int main(void) {
-	//물고기 정보 입력
-	for (int y = 0; y < 4; y++) {
-		for (int x = 0; x < 4; x++) {
+	//물고기 정보를 맵에 입력
+	for (int y = 1; y <= 4; y++) {
+		for (int x = 1; x <= 4; x++) {
 			int idx, dir;
 			scanf("%d %d", &idx, &dir);
-			map[y][x] = idx;
+			//맵에 해당 물고기 번호 삽입
+			board[y][x] = idx;
+			//배열에 물고기 정보입력
 			fish[idx] = { y,x,dir,1 };
 		}
 	}
