1a557141e364cb5404e0ef988582f72216be0cd8
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-25 15:19:46 +0900

    컨베이어 벨트 위의 로봇
    https://www.acmicpc.net/problem/20055

--- 20055_컨베이어 벨트 위의 로봇_deque.cpp
+++ 20055_컨베이어 벨트 위의 로봇_deque.cpp
@@ -4,79 +4,100 @@
 
 using namespace std;
 
+//벨트의 각 블록 
 struct BLOCK {
 	int d;
 	bool is_robot;
 };
 
-deque<BLOCK> dq;
+int n, k, result;
 
-int n, k;
-
-bool is_break_point(void) {
-	int zero_cnt = 0;
+//작동 중지 체크
+bool check_break(deque<BLOCK>& dq) {
+	int cnt = 0;
 	for (int i = 0; i < 2 * n; i++) {
 		if (dq[i].d == 0)
-			zero_cnt++;
+			cnt++;
 	}
-	if (zero_cnt >= k)
+	if (cnt >= k)
 		return true;
 	else
 		return false;
 }
 
-void rotate(void) {
+//벨트의 회전
+void rotate(deque<BLOCK>& dq) {
+	//덱 특성 이용
 	dq.push_front(dq.back());
 	dq.pop_back();
-
-	if (dq[n-1].is_robot)
+	//내리는 위치에 로봇이 존재하면
+	if (dq[n - 1].is_robot) {
 		dq[n - 1].is_robot = false;
+	}
+	return;
 }
 
-void move(void) {
+//로봇의 움직임
+void robot_move(deque<BLOCK>& dq) {
+	//1~3 ===> 0 1 2 구간만 회전
 	for (int i = n - 2; i >= 0; i--) {
 		if (dq[i].is_robot) {
 			if (!dq[i + 1].is_robot && dq[i + 1].d >= 1) {
+				//로봇이 이동가능
 				dq[i].is_robot = false;
 				dq[i + 1].is_robot = true;
 				dq[i + 1].d -= 1;
 			}
 		}
 	}
-	if (dq[n - 1].is_robot)
+	//다시 한번 내리는 위치에 로봇있나 확인
+	if (dq[n - 1].is_robot) {
 		dq[n - 1].is_robot = false;
+	}
+	return;
 }
 
-void input(void) {
-	if (!dq[0].is_robot && dq[0].d >= 1) {
-		dq[0].is_robot = true;
+//로봇 넣기
+void robot_input(deque<BLOCK>& dq) {
+	//내구성 1 이상이고 로봇이 없으면
+	if (dq[0].d >= 1 && !dq[0].is_robot) {
 		dq[0].d -= 1;
+		dq[0].is_robot = true;
 	}
 }
 
-int simulation(void) {
-	int play_step = 0;
+//시뮬레이션
+void simulation(deque<BLOCK>& dq) {
+	int play_cnt = 0;
+	//시뮬시작
 	while (true) {
-		if (is_break_point()) {
+		//작동해도되는 지 검사
+		if (check_break(dq))
 			break;
-		}
-		rotate();
-		move();
-		input();
-		play_step++;
+		//문제 조건 벨트와 로봇이 같이 회전
+		rotate(dq);
+		//로봇 이동
+		robot_move(dq);
+		//올리는 위치에 올리는지 마는지 확인
+		robot_input(dq);
+		play_cnt++;
 	}
-	return play_step;
+	result = play_cnt;
+	return;
 }
 
 int main(void) {
+	//입력
 	scanf("%d %d", &n, &k);
+	deque<BLOCK> dq;
+	//내구성 입력
 	for (int i = 0; i < 2 * n; i++) {
 		int d;
 		scanf("%d", &d);
 		dq.push_back({ d,false });
 	}
-	int ret = simulation();
-	printf("%d\n", ret);
+	//시뮬 시작
+	simulation(dq);
+	printf("%d", result);
 	return 0;
 }
-
