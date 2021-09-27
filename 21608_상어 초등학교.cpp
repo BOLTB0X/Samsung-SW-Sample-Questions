972623b3c317a3cbaeed1ead65d6f7eea899552c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-27 16:40:50 +0900

    상어 초등학교
    https://www.acmicpc.net/problem/21608

--- 21608_상어 초등학교.cpp
+++ 21608_상어 초등학교.cpp
@@ -1,5 +1,4 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
+#include <iostream>
 #include <queue>
 #include <vector>
 
@@ -9,144 +8,161 @@ using namespace std;
 struct STUDENT {
 	int student_idx;
 	int love[4];
-	int y, x;
+	int y;
+	int x;
 };
 
-//우선순위 큐를 위한 구조체
-struct INF {
-	int y, x, blank, love;
-	//우선순위 조건에 맞게 수정
-	bool operator < (const INF &i) const {
-		if (love == i.love) {
-			if (blank == i.blank) {
-				if (y == i.y)
+//우선순위를 위한 구조체
+struct INFO {
+	int y;
+	int x;
+	int blank_cnt;
+	int love_cnt;
+
+	bool operator < (const INFO& i) const {
+		//1순위 선호도
+		if (love_cnt == i.love_cnt) {
+			//2순위 빈공간
+			if (blank_cnt == i.blank_cnt) {
+				//행
+				if (y == i.y) {
 					return x > i.x;
+				}
+				//열
 				else
 					return y > i.y;
 			}
 			else
-				return blank < i.blank;
+				return blank_cnt < i.blank_cnt;
 		}
 		else
-			return love < i.love;
+			return love_cnt < i.love_cnt;
 	}
 };
 
-int n, result;
-int board[20][20] = { 0, };
-vector<STUDENT> students(400);
+//맵 정보
+int n,result;
+int board[20][20];
 
-//만족도 점수
-const int satisfy[5] = { 0,1,10,100,1000 };
-//상하좌우
+//연산 필요한 고정 배열
 const int dy[4] = { -1,1,0,0 };
 const int dx[4] = { 0,0,-1,1 };
+const int satisfy[5] = { 0,1,10,100,1000 };
 
-//상어 학생들의 자리배정
-void setting_sit(void) {
-	//기존 벡터에 저장된 학생들 뽑기
-	for (int i = 0; i < n * n; i++) {
-		//우선순위 큐 필요
-		priority_queue<INF> pq;
-		//그래프 탐색 시작
+//자리 배정
+//각 자리에대한 인근 선호도와 빈공간 수를 체크
+//그 후 우선순위 큐에 넣고 우선순위별로 뽑아 맵에 체크
+void setting_sit(vector<STUDENT>& student) {
+	//벡터에 담긴 학생이 기준
+	for (int i = 1; i <= n * n; i++) {
+		//이에 맞추어 pq선언
+		priority_queue<INFO> pq;
+		//맵 탐색
 		for (int y = 0; y < n; y++) {
 			for (int x = 0; x < n; x++) {
-				//만약 위치가 빈칸이라면
-				//이 위치에 따른 빈공간, 선호도를 알아야함
+				// 자리배정 가능한 칸을 만나면
 				if (board[y][x] == 0) {
-					//각 빈공간과 선호도 체크
-					int blank_cnt = 0, love_cnt = 0;
+					int near_blank = 0, near_love = 0;
+					//상하좌우를 탐색하며 인근 선호도, 빈공간 수 카운트
 					for (int dir = 0; dir < 4; dir++) {
 						int ny = y + dy[dir];
 						int nx = x + dx[dir];
-						//범위 초과시
-						if (nx < 0 || ny < 0 || ny >= n || nx >= n)
+						
+						//맵 초과
+						if (ny >= n || nx >= n || ny < 0 || nx < 0)
 							continue;
-						//빈공간 수 체크
+						//빈공간
 						if (board[ny][nx] == 0)
-							blank_cnt++;
-						//학생을 만난다면
+							near_blank++;
+						//학생을 보게 되면
 						else {
-							//지금 메인인 학생의 선호도학생들 탐색
-							for (int s = 0; s < 4; s++) {
-								if (board[ny][nx] == students[i].love[s]) {
-									love_cnt++;
+							for (int j = 0; j < 4; j++) {
+								if (board[ny][nx] == student[i].love[j]) {
+									near_love++;
 									break;
 								}
 							}
 						}
 					}
-					pq.push({ y,x,blank_cnt,love_cnt });
+					//삽입
+					pq.push({ y,x,near_blank,near_love });
 				}
 			}
 		}
-		//우선순위 큐가 비어있지않다면
+		//삽입이 되어진게 있으면 맵에 체크
 		if (!pq.empty()) {
-			//최상단 원소가 문제조건에 맞는 학생
 			int y = pq.top().y;
 			int x = pq.top().x;
-			//맵에 해당 학생 배치
-			board[y][x] = students[i].student_idx;
-			//해당 학생의 위치정보 벡터에 삽입
-			students[i].y = y;
-			students[i].x = x;
+			pq.pop();
+			board[y][x] = student[i].student_idx;
+			student[i].y = y;
+			student[i].x = x;
 		}
 	}
+	return;
 }
 
-//만족도 조사
-void get_satisfy_score(void) {
-	result = 0;
-	//각 학생의 만족도를 알아야함
-	for (int i = 0; i < n * n; i++) {
-		//대상인 학생의 위치 확인
-		int y = students[i].y;
-		int x = students[i].x;
-
-		//그 학생 주위 인접한 학생들 탐색
+//만족도 검사
+void get_satisfy(vector<STUDENT>& student) {
+	//벡터에 담긴 학생들 순회
+	for (int i = 1; i <= n * n; i++) {
+		int cy = student[i].y;
+		int cx = student[i].x;
 		int love = 0;
+		//인근 선호도 학생 체크
 		for (int dir = 0; dir < 4; dir++) {
-			int ny = y + dy[dir];
-			int nx = x + dx[dir];
-
-			//범위 초과시
-			if (nx < 0 || ny < 0 || ny >= n || nx >= n)
+			int ny = cy + dy[dir];
+			int nx = cx + dx[dir];
+			
+			//범위 초과
+			if (ny >= n || nx >= n || ny < 0 || nx < 0)
 				continue;
-			//그 학생들과 메인 학생의 선호도 배열과 확인
-			for (int k = 0; k < 4; k++) {
-				if (board[ny][nx] == students[i].love[k]) {
+			//선호학생이 있으면
+			for (int j = 0; j < 4; j++) {
+				if (board[ny][nx] == student[i].love[j]) {
 					love++;
 					break;
 				}
 			}
 		}
-		//총 love수만큼 
+		//만족도 결과에 더 해준다.
 		result += satisfy[love];
 	}
+	return;
 }
 
-//시뮬레이션 시작
-void simulation(void) {
-	setting_sit();
-	get_satisfy_score();
+//시뮬레이션 
+void simulation(vector<STUDENT>& student) {
+	setting_sit(student);
+	get_satisfy(student);
+	return;
 }
 
+//메인
 int main(void) {
-	//입력
-	scanf("%d", &n);
-	//상어 학생번호 입력
-	for (int i=0;i<n*n;i++) {
-		int idx;
-		scanf("%d", &idx);
-		students[i].student_idx = idx;
-		//선호 학생들 입력
-		for (int j = 0; j < 4; j++) {
-			int a;
-			scanf("%d", &a);
-			students[i].love[j] = a;
-		}
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	//벡터관련 입력
+	cin >> n;
+	//초기화
+	vector<STUDENT> student((n * n) + 1);
+	//학생 정보입력
+	for (int i = 1; i <= n * n; i++) {
+		int a, b, c, d, e;
+		cin >> a >> b >> c >> d >> e;
+		student[i].student_idx = a;
+		student[i].love[0] = b;
+		student[i].love[1] = c;
+		student[i].love[2] = d;
+		student[i].love[3] = e;
 	}
-	simulation();
-	printf("%d", result);
+	//연산을 위해 0으로 초기화
+	result = 0;
+	//시뮬시작
+	simulation(student);
+	cout << result << '\n';
 	return 0;
 }
