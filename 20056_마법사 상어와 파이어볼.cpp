106e3301695755e7705df0e0659b177603b069eb
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-29 15:41:56 +0900

    마법사 상어와 파이어볼
    https://www.acmicpc.net/problem/20056

--- 20056_마법사 상어와 파이어볼.cpp
+++ 20056_마법사 상어와 파이어볼.cpp
@@ -1,138 +1,153 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdio> 
-#include <vector> 
+#include <iostream>
+#include <vector>
 
-using namespace std; 
+using namespace std;
 
-//파이어볼 자료형 정의
-typedef struct {
-	int y, x;
-	int mess, s, d;
-} INF;
+//파이어볼의 정보를 담는 구조체
+struct FIREBALL {
+	int y, x, mess, speed, dir;
+};
 
-int n, m, k;
-//맵을 벡터로 한 셀에 여러 원소가 올수 있음
-vector<INF> map[51][51];
-vector<INF> fireball;
+int n, m, k, result;
+vector<FIREBALL> board[51][51];
+vector<FIREBALL> fireball;
 
-//이동방향 
 const int dy[8] = { -1,-1,0,1,1,1,0,-1 };
 const int dx[8] = { 0,1,1,1,0,-1,-1,-1 };
 
-//파이어볼 정보에 따라 이동
-void fireball_move(void) {
-	//기존 맵 초기화
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			map[i][j].clear();
+//맵 초기화
+void board_init(void) {
+	for (int y = 1; y <= n; y++) {
+		for (int x = 1; x <= n; x++) {
+			board[y][x].clear();
 		}
 	}
-	//파이어볼 이동
+}
+
+//파이어볼 이동
+void fireball_move(void) {
+	board_init();
 	for (int i = 0; i < fireball.size(); i++) {
-		INF cur_fireball = fireball[i];
-		int cur_y = cur_fireball.y;
-		int cur_x = cur_fireball.x;
-		int ny = cur_y + dy[cur_fireball.d] * cur_fireball.s;
-		int nx = cur_x + dx[cur_fireball.d] * cur_fireball.s;
-		//
+		FIREBALL cur_fireball = fireball[i];
+		int cy = cur_fireball.y;
+		int cx = cur_fireball.x;
+
+		//파이어볼 이동
+		int ny = cy + dy[cur_fireball.dir] * cur_fireball.speed;
+		int nx = cx + dx[cur_fireball.dir] * cur_fireball.speed;
+		//범위 검사
+		//너무 깊게 들어갔을 때
 		while (ny < 1)
 			ny += n;
 		while (nx < 1)
 			nx += n;
-		while (ny > n)
-			ny -= n;
 		while (nx > n)
 			nx -= n;
-		map[ny][nx].push_back({ ny,nx,cur_fireball.mess,cur_fireball.s,cur_fireball.d });
+		while (ny > n)
+			ny -= n;
+		//파이어볼이 이동했으니 맵에 표시해줌
+		board[ny][nx].push_back({ ny,nx,cur_fireball.mess,cur_fireball.speed,cur_fireball.dir });
 	}
 	return;
 }
 
-// 방향이 모두 홀수인지, 모두 짝수인지 판별
-bool is_direction(int y, int x) {
-	bool even_flag = false, odd_flag = false;
-	for (int i = 0; i < map[y][x].size(); i++) {
-		if (map[y][x][i].d % 2 == 0)
+//한 맵의 셀에 파이어볼의 방향 확인
+bool is_odd_even(int y, int x) {
+	bool odd_flag = false;
+	bool even_flag = false;
+
+	for (int i = 0; i < board[y][x].size(); i++) {
+		if (board[y][x][i].dir % 2 == 0)
 			even_flag = true;
 		else
 			odd_flag = true;
 	}
-	//검사
-	if (even_flag && !odd_flag) 
+	if (even_flag && !odd_flag)
 		return true;
-	else if (!even_flag && odd_flag) 
+	else if (!even_flag && odd_flag)
 		return true;
-	else 
+	else
 		return false;
 }
 
-//fire볼 업데이트
+//파이어볼 업데이트
 void fireball_update(void) {
-	vector<INF> tmp;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= n; j++) {
-			int mass_tot = 0, speed_tot = 0;
+	//임시 파이어볼 벡터
+	vector<FIREBALL> tmp_fireball;
+	//파이어볼이 2개인지 확인
+	for (int y = 1; y <= n; y++) {
+		for (int x = 1; x <= n; x++) {
 			//비어있다면
-			if (map[i][j].size() == 0)
+			if (board[y][x].size() == 0)
 				continue;
-			//1개면 삽입
-			if (map[i][j].size() == 1)
-				tmp.push_back(map[i][j][0]);
-			//2개 이상이면
+			//1개 있을 경우
+			else if (board[y][x].size() == 1) {
+				tmp_fireball.push_back(board[y][x][0]);
+			}
+			//2개 이상
+			//파이어볼을 합쳐야함
 			else {
-				for (int k = 0; k < map[i][j].size(); k++) {
-					mass_tot += map[i][j][k].mess;
-					speed_tot += map[i][j][k].s;
+				int tot_mess = 0, tot_speed = 0;
+				for (int i = 0; i < board[y][x].size(); i++) {
+					tot_mess += board[y][x][i].mess;
+					tot_speed += board[y][x][i].speed;
 				}
-				int avg_mas = mass_tot / 5;
-				int avg_speed = speed_tot / map[i][j].size();
-				if (avg_mas == 0)
+				int avg_mess = tot_mess / 5;
+				if (avg_mess == 0) {
 					continue;
-				// all 홀 인지 all 짝인 검사
-				if (is_direction(i, j)) {
-					tmp.push_back({ i,j,avg_mas,avg_speed,0 });
-					tmp.push_back({ i,j,avg_mas,avg_speed,2 });
-					tmp.push_back({ i,j,avg_mas,avg_speed,4 });
-					tmp.push_back({ i,j,avg_mas,avg_speed,6 });
+				}
+				int avg_speed = tot_speed / board[y][x].size();
+
+				//합쳐지는 파이어볼 방향 홀짝인지 검사
+				if (is_odd_even(y, x)) {
+					tmp_fireball.push_back({ y,x,avg_mess,avg_speed,0 });
+					tmp_fireball.push_back({ y,x,avg_mess,avg_speed,2 });
+					tmp_fireball.push_back({ y,x,avg_mess,avg_speed,4 });
+					tmp_fireball.push_back({ y,x,avg_mess,avg_speed,6 });
 				}
 				else {
-					tmp.push_back({ i,j,avg_mas,avg_speed,1 });
-					tmp.push_back({ i,j,avg_mas,avg_speed,3 });
-					tmp.push_back({ i,j,avg_mas,avg_speed,5 });
-					tmp.push_back({ i,j,avg_mas,avg_speed,7 });
+					tmp_fireball.push_back({ y,x,avg_mess,avg_speed,1 });
+					tmp_fireball.push_back({ y,x,avg_mess,avg_speed,3 });
+					tmp_fireball.push_back({ y,x,avg_mess,avg_speed,5 });
+					tmp_fireball.push_back({ y,x,avg_mess,avg_speed,7 });
 				}
 			}
 		}
 	}
-	//벡터교체
-	fireball = tmp;
+	//수정한 벡터 기존벡터로 교체
+	fireball = tmp_fireball;
 }
 
-//시뮬레이션 실행
-int simulation(void) {
-	//시뮬시작
-	for (int i = 0; i < k; i++) {
+//시뮬레이션
+void simulation(void) {
+	//k번 반복
+	while (k--) {
 		fireball_move();
 		fireball_update();
 	}
-	//질량출력
-	int tot = 0;
+	//정답 도출
 	for (int i = 0; i < fireball.size(); i++) {
-		tot += fireball[i].mess;
+		result += fireball[i].mess;
 	}
-	return tot;
+	return;
 }
 
 int main(void) {
-	//입력
-	scanf("%d %d %d", &n, &m, &k);
-	//파이어볼 정보 입력
-	for (int i = 1; i <= m; i++) {
-		int y, x, mess, d, s;
-		scanf("%d %d %d %d %d", &y, &x, &mess, &s, &d);
-		fireball.push_back({ y,x,mess,s,d });
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	
+	//
+	cin >> n >> m >> k;
+	for (int i = 0; i < m; i++) {
+		int a, b, c, d, e;
+		cin >> a >> b >> c >> d >> e;
+		fireball.push_back({ a,b,c,d,e });
 	}
-	int ret = simulation();
-	printf("%d", ret);
+	//시뮬 시작
+	result = 0;
+	simulation();
+	cout << result << '\n';
 	return 0;
 }
