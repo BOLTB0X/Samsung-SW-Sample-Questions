7f3573582be7d52b557a7cdaf6191745896ce849
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-09-30 17:36:05 +0900

    컨베이어 벨트 위의 로봇
    https://www.acmicpc.net/problem/20055

--- 20055_컨베이어 벨트 위의 로봇_deque.cpp
+++ 20055_컨베이어 벨트 위의 로봇_deque.cpp
@@ -1,103 +1,99 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <cstdio>
-#include <queue>
+#include <iostream>
+#include <deque>
 
 using namespace std;
 
-//벨트의 각 블록 
+//벨트의 한 블록
 struct BLOCK {
 	int d;
 	bool is_robot;
 };
 
-int n, k, result;
+int n, k;
+deque<BLOCK> blets;
 
-//작동 중지 체크
-bool check_break(deque<BLOCK>& dq) {
-	int cnt = 0;
+//중지는 시키는 지 판단 함수
+bool is_break(void) {
+	int zero_cnt = 0;
 	for (int i = 0; i < 2 * n; i++) {
-		if (dq[i].d == 0)
-			cnt++;
+		if (blets[i].d == 0)
+			zero_cnt++;
 	}
-	if (cnt >= k)
+	//중지 조건이면
+	if (zero_cnt >= k)
 		return true;
-	else
-		return false;
+	return false;
 }
 
-//벨트의 회전
-void rotate(deque<BLOCK>& dq) {
-	//덱 특성 이용
-	dq.push_front(dq.back());
-	dq.pop_back();
-	//내리는 위치에 로봇이 존재하면
-	if (dq[n - 1].is_robot) {
-		dq[n - 1].is_robot = false;
-	}
+//벨트 회전 
+void blets_rotate(void) {
+	//한칸 씩 뒤로 밀수있음
+	blets.push_front(blets.back());
+	blets.pop_back();
+
+	//조건없이 내리는 위치
+	if (blets[n - 1].is_robot)
+		blets[n - 1].is_robot = false;
 	return;
 }
 
-//로봇의 움직임
-void robot_move(deque<BLOCK>& dq) {
-	//1~3 ===> 0 1 2 구간만 회전
+// 0 1 2 이 칸들은 로봇들이 직접 이동
+void robot_move(void) {
 	for (int i = n - 2; i >= 0; i--) {
-		if (dq[i].is_robot) {
-			if (!dq[i + 1].is_robot && dq[i + 1].d >= 1) {
-				//로봇이 이동가능
-				dq[i].is_robot = false;
-				dq[i + 1].is_robot = true;
-				dq[i + 1].d -= 1;
+		//로봇이 있지만
+		if (blets[i].is_robot) {
+			//다음칸에 로봇이 없고 내구성이 1이상이면
+			if (!blets[i + 1].is_robot && blets[i + 1].d >= 1) {
+				blets[i].is_robot = false;
+				blets[i + 1].is_robot = true;
+				blets[i + 1].d -= 1;
 			}
 		}
 	}
-	//다시 한번 내리는 위치에 로봇있나 확인
-	if (dq[n - 1].is_robot) {
-		dq[n - 1].is_robot = false;
+	//조건없이 내리는 위치
+	if (blets[n - 1].is_robot)
+		blets[n - 1].is_robot = false;
+	return;
+}
+
+//0은 로봇을 올리는 위치
+void robot_input(void) {
+	//로봇이 없고 내구성이 1이상면
+	if (blets[0].d >= 1 && !blets[0].is_robot) {
+		blets[0].d -= 1;
+		blets[0].is_robot = true;
 	}
 	return;
 }
 
-//로봇 넣기
-void robot_input(deque<BLOCK>& dq) {
-	//내구성 1 이상이고 로봇이 없으면
-	if (dq[0].d >= 1 && !dq[0].is_robot) {
-		dq[0].d -= 1;
-		dq[0].is_robot = true;
-	}
-}
-
-//시뮬레이션
-void simulation(deque<BLOCK>& dq) {
+//시뮬
+int simulation(void) {
 	int play_cnt = 0;
-	//시뮬시작
 	while (true) {
-		//작동해도되는 지 검사
-		if (check_break(dq))
+		if (is_break())
 			break;
-		//문제 조건 벨트와 로봇이 같이 회전
-		rotate(dq);
-		//로봇 이동
-		robot_move(dq);
-		//올리는 위치에 올리는지 마는지 확인
-		robot_input(dq);
+		blets_rotate();
+		robot_move();
+		robot_input();
 		play_cnt++;
 	}
-	result = play_cnt;
-	return;
+	return play_cnt;
 }
 
 int main(void) {
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+
 	//입력
-	scanf("%d %d", &n, &k);
-	deque<BLOCK> dq;
-	//내구성 입력
+	cin >> n >> k;
 	for (int i = 0; i < 2 * n; i++) {
 		int d;
-		scanf("%d", &d);
-		dq.push_back({ d,false });
+		cin >> d;
+		blets.push_back({ d,false });
 	}
-	//시뮬 시작
-	simulation(dq);
-	printf("%d", result);
+	int ret = simulation();
+	cout << ret << '\n';
 	return 0;
 }
