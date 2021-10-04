75e4be4ff6ebed599ddbdee8ffb321015b5e3e1e
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-04 16:37:48 +0900

    로봇 청소기
    https://www.acmicpc.net/problem/14503

--- 14503_로봇청소기.cpp
+++ 14503_로봇청소기.cpp
@@ -1,88 +1,81 @@
 #include <iostream>
 #include <queue>
+
 using namespace std;
 
-struct pos {
-	int r;
-	int c;
-	int dir;
+//로봇
+struct ROBOT{
+	int y, x, dir;
 };
 
 int n, m;
-int map[51][51];
-queue<pos> q;
-//  0
-// 3 1
-//  2
-int turn_left(int d) {
-	if (d == 0)
+int board[50][50];
+
+//동서남북
+const int dy[4] = { -1,0,1,0 };
+const int dx[4] = { 0,1,0,-1 };
+
+//왼쪽으로 회전
+int turn_left(int dir) {
+	if (dir == 0)
 		return 3;
-	else if (d == 1)
-		return 0;
-	else if (d == 2)
-		return 1;
-	else if (d == 3)
-		return 2;
+	return dir - 1;
 }
 
-int turn_back(int d) {
-	if (d == 0)
+//뒤로 회전
+int turn_back(int dir) {
+	if (dir == 0)
 		return 2;
-	else if (d == 1)
-		return 3;
-	else if (d == 2)
+	else if (dir == 2)
 		return 0;
-	else if (d == 3)
+	else if (dir == 3)
 		return 1;
+	else if (dir == 1)
+		return 3;
 }
 
-int BFS(int row, int col, int direction) {
-	int dr[4] = { -1,0,1,0 };
-	int dc[4] = { 0,1,0,-1 };
-	pos start;
-	start.r = row;
-	start.c = col;
-	start.dir = direction;
-	q.push(start);
+//범위 체크
+bool is_range(int y, int x) {
+	if (y < 0 || x < 0 || y >= n || x >= m)
+		return false;
+	return true;
+}
+
+//너비우선
+int BFS(int y, int x, int dir) {
+	queue<ROBOT> q;
+	q.push({ y,x,dir });
+	board[y][x] = 2;
 	int result = 1;
-	map[start.r][start.c] = 2;
-	//2번
+	
+	//큐가 빌때까지
 	while (!q.empty()) {
-		pos cur;
-		cur.r = q.front().r;
-		cur.c = q.front().c;
-		cur.dir = q.front().dir;
+		ROBOT cur = q.front();
 		int tmp_dir = cur.dir;
 		q.pop();
-		
-		for (int i = 0; i < 4; i++) {
+		//동서남북 ==> 4번 
+		for (int dir = 0; dir < 4; dir++) {
+			//왼쪽부터 차례로
 			tmp_dir = turn_left(tmp_dir);
-			int nr = cur.r + dr[tmp_dir];
-			int nc = cur.c + dc[tmp_dir];
-
-			//2번의 a
-			if (0 <= nr && nr < n && 0 <= nc && nc < m && map[nr][nc] == 0) {
-				pos next;
-				next.r = nr;
-				next.c = nc;
-				next.dir = tmp_dir;
-				map[next.r][next.c] = 2;
-				q.push(next);
-				result += 1;
-				break;
-			}
-			//if에 안걸리면 2번의 b
-			
-			//2번의 c
-			else if (i == 3) {
-				pos back;
-				back.r = cur.r + dr[turn_back(cur.dir)];
-				back.c = cur.c + dc[turn_back(cur.dir)];
-				back.dir = cur.dir;
-				q.push(back);
-
-				if (map[back.r][back.c] == 1) {
-					return result;
+			int ny = cur.y + dy[tmp_dir];
+			int nx = cur.x + dx[tmp_dir];
+			if (is_range(ny, nx)) {
+				if (board[ny][nx] == 0) {
+					board[ny][nx] = 2;
+					q.push({ ny,nx,tmp_dir });
+					result++;
+					break;
+				}
+				//다 진행했는데도 갈곳이 없다면
+				else if (dir == 3) {
+					int ny = cur.y + dy[turn_back(cur.dir)];
+					int nx = cur.x + dx[turn_back(cur.dir)];
+					if (is_range(ny, nx)) {
+						if (board[ny][nx] == 1)
+							return result;
+						else
+							q.push({ ny,nx,cur.dir });
+					}
 				}
 			}
 		}
@@ -91,22 +84,22 @@ int BFS(int row, int col, int direction) {
 }
 
 int main(void) {
+	//초기화
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 	cout.tie(0);
-
+	//입력
+	int y, x, dir;
 	cin >> n >> m;
-	int row, col, direction;
-	cin >> row >> col >> direction;
+	cin >> y >> x >> dir;
 
-	for (int r = 0; r < n; r++) {
-		for (int c = 0; c < m; c++) {
-			cin >> map[r][c];
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < m; x++) {
+			cin >> board[y][x];
 		}
 	}
-	
-	int ret = BFS(row,col,direction);
-
+	// BFS시뮬레이션 시작
+	int ret = BFS(y, x, dir);
 	cout << ret << '\n';
 	return 0;
 }
