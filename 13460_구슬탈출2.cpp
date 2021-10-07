d9cd00da275fb49d781cccf92d12696ea9475904
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-07 13:46:08 +0900

    구슬 탈출 2
    https://www.acmicpc.net/problem/13460

--- 13460_구슬탈출2.cpp
+++ 13460_구슬탈출2.cpp
@@ -1,133 +1,154 @@
-#include <cstdio>
+#include <iostream>
 #include <queue>
-#include <algorithm>
+
 using namespace std;
 
-struct bead {
-	int ry, rx, by, bx, cnt;
-};
+//구슬
+typedef struct {
+	int red_y;
+	int red_x;
+	int blue_y;
+	int blue_x;
+	int sec;
+}BEAD;
 
-bead start;
 int n, m;
-char map[10][11];
+char board[10][11];
+//두개의 구슬, y,x이므로
+bool visited[10][10][10][10];
 
-int BFS() {
-	const int dx[4] = { -1,0,1,0 };
-	const int dy[4] = { 0,1,0,-1 };
-	bool visited[10][10][10][10] = { false, };
-	queue<bead> que;
-	que.push(start);
-	visited[start.ry][start.rx][start.by][start.bx] = true;
+//상하좌우
+const int dy[4] = { 1,-1,0,0 };
+const int dx[4] = { 0,0,-1,1 };
 
-	int ret = -1;
-	while (!que.empty()) {
-		bead cur = que.front();
-		que.pop();
+//절댓값
+int abso(int a) {
+	if (a < 0)
+		return -a;
+	return a;
+}
 
-		if (cur.cnt > 10) {
+//너비우선
+int BFS(BEAD start) {
+	queue<BEAD> q;
+	q.push(start);
+	//초기 방문처리
+	visited[start.red_y][start.red_x][start.blue_y][start.blue_x] = true;
+	int res = -1;
+
+	while (!q.empty()) {
+		BEAD cur = q.front();
+		q.pop();
+
+		//탈출조건 2개
+		//1개 10초 초과인 경우
+		if (cur.sec > 10) 
+			break;
+		//빨간구슬이 구멍에 빠질 경우 
+		//파란 구슬은 애초에 구멍에 안가게 함
+		if (board[cur.red_y][cur.red_x] == 'O' && board[cur.blue_y][cur.blue_x] != 'O') {
+			res = cur.sec;
 			break;
 		}
 
-		if (map[cur.ry][cur.rx] == 'O' && map[cur.by][cur.bx] != 'O') {
-			ret = cur.cnt;
-			break;
-		}
-
+		//상하좌우 차레로 탐색
 		for (int dir = 0; dir < 4; dir++) {
-			int move_ry = cur.ry;
-			int move_rx = cur.rx;
-			int move_by = cur.by;
-			int move_bx = cur.bx;
+			int nry = cur.red_y;
+			int nrx = cur.red_x;
+			int nby = cur.blue_y;
+			int nbx = cur.blue_x;
 
-			//공움직임
+			//기울기 이용이므로 무한이용
+			//빨간구슬
 			while (true) {
-				//빨간공 움직임
-				if (move_ry >= 0 && move_ry < n && move_rx >= 0 && move_rx < m) {
-					if (map[move_ry][move_rx] != '#' && map[move_ry][move_rx] != 'O') {
-						move_ry += dy[dir];
-						move_rx += dx[dir];
+				//빈공간인 경우 
+				if (board[nry][nrx] != 'O' && board[nry][nrx] != '#') {
+					nry += dy[dir];
+					nrx += dx[dir];
+				}
+				//그외 즉 구멍이던가 벽이던가
+				else {
+					if (board[nry][nrx] == '#') {
+						nry -= dy[dir];
+						nrx -= dx[dir];
 					}
-					else {
-						if (map[move_ry][move_rx] == '#') {
-							move_ry -= dy[dir];
-							move_rx -= dx[dir];
-						}
-						break;
-					}
+					break;
 				}
 			}
+			//파란구슬
 			while (true) {
-				//파란공 움직임
-				if (move_by >= 0 && move_by < n && move_bx >= 0 && move_bx < m) {
-					if (map[move_by][move_bx] != '#' && map[move_by][move_bx] != 'O') {
-						move_by += dy[dir];
-						move_bx += dx[dir];
+				//빈공간
+				if (board[nby][nbx] != 'O' && board[nby][nbx] != '#') {
+					nby += dy[dir];
+					nbx += dx[dir];
+				}
+				//그외
+				else {
+					if (board[nby][nbx] == '#') {
+						nby -= dy[dir];
+						nbx -= dx[dir];
 					}
-					else {
-						if (map[move_by][move_bx] == '#') {
-							move_by -= dy[dir];
-							move_bx -= dx[dir];
-						}
-						break;
-					}
+					break;
 				}
 			}
-			//공의 위치가 겹칠 경우
-			if (move_ry == move_by && move_rx == move_bx) {
-				if (map[move_ry][move_rx] != 'O') {
-					int r_dist = abs(move_ry - cur.ry) + abs(move_rx - cur.rx);
-					int b_dist = abs(move_by - cur.by) + abs(move_bx - cur.bx);
+			//구슬이 겹쳤을 경우
+			if (nby == nry && nbx == nrx) {
+				//구멍이 아니라면
+				if (board[nry][nrx] != 'O') {
+					int r_dist = abso(nry - cur.red_y) + abso(nrx - cur.red_x);
+					int b_dist = abso(nby - cur.blue_y) + abso(nbx - cur.blue_x);
 
 					if (r_dist > b_dist) {
-						move_ry -= dy[dir];
-						move_rx -= dx[dir];
+						nry -= dy[dir];
+						nrx -= dx[dir];
 					}
-					else {
-						move_by -= dy[dir];
-						move_bx -= dx[dir];
+
+					else if (b_dist > r_dist) {
+						nby -= dy[dir];
+						nbx -= dx[dir];
 					}
 				}
 			}
-			//이동을 다 했으니 방문 처리
-			if (visited[move_ry][move_rx][move_by][move_bx] == false) {
-				visited[move_ry][move_rx][move_by][move_bx] = true;
-				bead move;
-				move.ry = move_ry;
-				move.rx = move_rx;
-				move.by = move_by;
-				move.bx = move_bx;
-				move.cnt = cur.cnt + 1;
-				que.push(move);
+			//방문처리
+			if (!visited[nry][nrx][nby][nbx]) {
+				visited[nry][nrx][nby][nbx] = true;
+				q.push({ nry,nrx,nby,nbx,cur.sec + 1 });
 			}
 		}
 	}
-	return ret;
+
+	return res;
+}
+
+void simulation(void) {
+	BEAD start;
+	//입력
+	cin >> n >> m;
+	for (int y = 0; y < n; y++) {
+		for (int x = 0; x < m; x++) {
+			cin >> board[y][x];
+			if (board[y][x] == 'R') {
+				start.red_y = y;
+				start.red_x = x;
+			}
+			else if (board[y][x] == 'B') {
+				start.blue_y = y;
+				start.blue_x = x;
+			}
+		}
+	}
+	start.sec = 0;
+
+	int ret = BFS(start);
+	cout << ret << '\n';
 }
 
 int main(void) {
-	scanf("%d %d", &n, &m);
+	//초기화
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
 
-	for (int i = 0; i < n; i++) {
-		scanf("%s", map[i]);
-	}
-
-	for (int y = 0; y < n; y++) {
-		for (int x = 0; x < m; x++) {
-			//빨간 공 찾기
-			if (map[y][x] == 'R') {
-				start.ry = y;
-				start.rx = x;
-			}
-			//파란 공 찾기
-			if (map[y][x] == 'B') {
-				start.by = y;
-				start.bx = x;
-			}
-		}
-	}
-	start.cnt = 0;
-
-	int ret = BFS();
-	printf("%d\n", ret);
+	simulation();
 	return 0;
 }
