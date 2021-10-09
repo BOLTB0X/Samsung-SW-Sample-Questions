fa669d58f3c7b88fe89c7e212450dcfb4798612c
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-09 16:41:23 +0900

    연산자 끼워넣기
    https://www.acmicpc.net/problem/14888

--- 14888_연산자 끼워넣기.c
+++ 14888_연산자 끼워넣기.c
@@ -1,6 +1,8 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 
+int n, max_result = -0x7fffffff, min_result = 0x7fffffff;
+
 //최대
 int max(int a, int b) {
 	return a > b ? a : b;
@@ -10,44 +12,47 @@ int min(int a, int b) {
 	return a < b ? a : b;
 }
 
-int n, max_result = -0x7fffffff, min_result = 0x7fffffff;
-
-//DFS
-void DFS(int *arr,int tot, int add, int sub, int mul, int div, int idx) {
-	//특정 깊이에 도달하면 스탑
+//깊이우선을 이용하는 백트래킹
+void DFS(int* arr, int tot, int add, int sub, int mul, int div, int idx) {
+	//탈출조건
 	if (idx == n) {
 		max_result = max(max_result, tot);
 		min_result = min(min_result, tot);
 		return;
 	}
+	//그 이외
 	else {
 		if (add > 0) {
-			DFS(arr,tot + arr[idx], add - 1, sub, mul, div, idx + 1);
+			DFS(arr, tot + arr[idx], add - 1, sub, mul, div, idx + 1);
 		}
 		if (sub > 0) {
-			DFS(arr,tot - arr[idx], add, sub - 1, mul, div, idx + 1);
+			DFS(arr, tot - arr[idx], add, sub - 1, mul, div, idx + 1);
 		}
 		if (mul > 0) {
-			DFS(arr,tot * arr[idx], add, sub, mul - 1, div, idx + 1);
+			DFS(arr, tot * arr[idx], add, sub, mul - 1, div, idx + 1);
 		}
 		if (div > 0) {
-			DFS(arr,(int)tot / arr[idx], add, sub, mul, div - 1, idx + 1);
+			DFS(arr, (int)tot / arr[idx], add, sub, mul, div - 1, idx + 1);
 		}
 	}
 }
 
 int main(void) {
 	int numbers[11];
+
 	//입력
 	scanf("%d", &n);
-	for (int i = 0; i < n; i++) 
+	for (int i = 0; i < n; i++)
 		scanf("%d", &numbers[i]);
 
 	int add, sub, mul, div;
-
-	scanf("%d %d %d %d", &add, &sub, &mul, &div);
-	DFS(numbers,numbers[0], add, sub, mul, div, 1);
 	
+	//연산자들 입력
+	scanf("%d %d %d %d", &add, &sub, &mul, &div);
+	//DFS 탐새시작
+	DFS(numbers, numbers[0], add, sub, mul, div, 1);
+
+	//정답 도출
 	printf("%d\n", max_result);
 	printf("%d\n", min_result);
 	return 0;
