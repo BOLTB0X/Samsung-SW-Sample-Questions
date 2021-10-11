d83b880beff99f91ce6f024d94daa62d4e416f8d
BOLTB0X <83914919+BOLTB0X@users.noreply.github.com>
2021-10-11 17:26:27 +0900

    아기 상어
    https://www.acmicpc.net/problem/16236

--- 16236_아기 상어.cpp
+++ 16236_아기 상어.cpp
@@ -3,86 +3,116 @@
 
 using namespace std;
 
-//상어 정보를 담는 구조체
 struct SHARK {
-	int dist, y, x;
+	int y, x, dist;
 
-	//문제조건
 	bool operator < (const SHARK& s) const {
 		if (dist == s.dist) {
-			if (y == s.y)
+			if (y == s.y) {
 				return x > s.x;
+			}
 			return y > s.y;
 		}
 		return dist > s.dist;
 	}
 };
 
-int n;
+int n, result;
 int board[20][20];
 bool visited[20][20];
+//아기상어의 움직임을 담는 우선순위 큐
+priority_queue<SHARK> pq;
 
-//하상좌우
-const int dy[4] = { -1,1,0,0 };
+//상하좌우
+const int dy[4] = { 1,-1,0,0 };
 const int dx[4] = { 0,0,-1,1 };
 
-//초기화
-void init(priority_queue<SHARK>& pq) {
-	//방문 초기화
-	for (int y = 0; y < n; y++)
-		for (int x = 0; x < n; x++)
-			visited[y][x] = false;
-	while (!pq.empty())
-		pq.pop();
+//입력
+void input(void) {
+	cin >> n;
+	for (int y = 0; y < n; ++y) {
+		for (int x = 0; x < n; ++x) {
+			cin >> board[y][x];
+			//아기 상어가 입력된다면
+			if (board[y][x] == 9) {
+				pq.push({ y,x,0 });
+				board[y][x] = 0;
+			}
+		}
+	}
 	return;
 }
 
-//BFS
-int BFS(priority_queue<SHARK>& pq) {
-	//초기값 세팅
-	int move = 0;
-	int shark_size = 2;
-	int eat_cnt = 0;
+//우선순위 큐와 방문리스트 초기화
+void init(void) {
+	while (!pq.empty()) {
+		pq.pop();
+	}
+	for (int y = 0; y < n; ++y) {
+		for (int x = 0; x < n; ++x) {
+			visited[y][x] = false;
+		}
+	}
+	return;
+}
 
-	while (!pq.empty())	{
+//너비 우선 탐색
+void BFS(void) {
+	int shark_size = 2;
+	int shark_move = 0;
+	int eating_cnt = 0;
+
+	//큐가 빌때까지
+	while (!pq.empty()) {
 		SHARK cur = pq.top();
 		pq.pop();
 		
-		//잡아 먹을 수 있다면 0보다 큰 조건을 걸지 않으면 빈칸도 잡아버림
-		if (shark_size > board[cur.y][cur.x] && board[cur.y][cur.x] > 0) {
-			eat_cnt++;
-			board[cur.y][cur.x] = 0;
-			//사이즈업 조건
-			if (shark_size == eat_cnt) {
-				shark_size++;
-				eat_cnt = 0;
+		//물고기가 있다면
+		if (board[cur.y][cur.x] > 0) {
+			//잡아먹을 수 있다면
+			//상어 사이즈보다 미만인 경우
+			if (shark_size > board[cur.y][cur.x]) {
+				eating_cnt++;
+				board[cur.y][cur.x] = 0;
+				//사이즈 업이 가능하다면
+				if (eating_cnt == shark_size) {
+					shark_size++;
+					eating_cnt = 0;
+				}
+				//이동거리가 즉 소요시간
+				shark_move += cur.dist;
+				//pq및 방문리스트 초기화
+				init();
+				cur.dist = 0;
 			}
-			//거리가 즉 총 걸린 시간이므로 더해주고
-			move += cur.dist;
-			//다 초기화
-			cur.dist = 0;
-			init(pq);
+
 		}
-		//cur위치에서 새로운 물고기를 잡기 위해 BFS탐색
-		for (int dir = 0; dir < 4; dir++) {
+		//상하좌우 탐색
+		for (int dir = 0; dir < 4; ++dir) {
 			int ny = cur.y + dy[dir];
 			int nx = cur.x + dx[dir];
-			
+
 			//범위 초과
-			if (ny >= n || nx >= n || ny < 0 || nx < 0)
+			if (ny < 0 || nx < 0 || ny >= n || nx >= n)
 				continue;
-			//재방문
-			if (visited[ny][nx])
+			//재방문이고 상어사이즈보다 큰 물고기가 있다면
+			if (visited[ny][nx] || board[ny][nx] > shark_size)
 				continue;
-			//몸집이 크면
-			if (board[ny][nx] > shark_size)
-				continue;
-			//pq삽입
-			pq.push({ cur.dist + 1,ny,nx });
+			//현재 위치 우선순위 삽입
+			pq.push({ ny,nx,cur.dist + 1 });
 			visited[ny][nx] = true;
 		}
 	}
-	return move;
+	result = shark_move;
+	return;
+}
+
+//시뮬레이션 시작
+void simulation(void) {
+	input();
+	BFS();
+	cout << result << '\n';
+	return;
 }
 
 int main(void) {
@@ -91,22 +121,7 @@ int main(void) {
 	cin.tie(0);
 	cout.tie(0);
 
-	//입력
-	cin >> n;
-	priority_queue<SHARK> pq;
-	//맵 생성
-	for (int y = 0; y < n; y++) {
-		for (int x = 0; x < n; x++) {
-			cin >> board[y][x];
-			//상어 위치 찾기
-			if (board[y][x] == 9) {
-				pq.push({ 0,y,x});
-				board[y][x] = 0;
-			}
-		}
-	}
-	//BFS시작
-	int ret = BFS(pq);
-	cout << ret << '\n';
+	//시뮬 시작
+	simulation();
 	return 0;
 }
